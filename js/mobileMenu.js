8c30155ba2641e5b678f208c5a837e4ddfb4ee70
dawkzor <89469714+dawkk@users.noreply.github.com>
2021-12-23 16:57:35 -0300

    after clicking on the search icon, now an close Icon appears and Menu disappears

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -17,16 +17,26 @@ btnMenuClose.addEventListener('click', mobileMenu);
 
 const btnSearch = document.querySelector('[btn-mobile-search]');
 const search = document.querySelector('[search-bar]');
+const btnCloseSearch = document.querySelector('[btn-mobile-close-search]');
 
 
 btnSearch.addEventListener('click', mobileSearch);
+btnCloseSearch.addEventListener('click', mobileSearch);
 
 function mobileSearch() {
-    if (search.style.display == 'flex') { search.style.display = 'none';
+    if (search.style.display == 'flex') { 
+     search.style.display = 'none';
     logo.style.display = 'flex';
+    btnCloseSearch.style.display = 'none';
+    btnSearch.style.display = 'flex';
+    btnMenuOpen.style.display = 'flex';
+    
     } else {
         search.style.display = 'flex';
         logo.style.display = 'none';
+        btnCloseSearch.style.display = 'flex';
+        btnSearch.style.display = 'none';
+        btnMenuOpen.style.display = 'none';
     }
 }
 
